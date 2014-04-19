fb0226ffaf11747bb9b67ef26c37d6c1f560c5d5
Tim Popham <popham@uw.edu>
2014-04-19 16:43:32 -0700

    Removed std namespace injection and replaced with range_pair concept

--- src/worker/graph.hpp
+++ src/worker/graph.hpp
@@ -4,18 +4,28 @@
 
 #include <boost/graph/adjacency_list.hpp>
 
+struct VProperties {
+  unsigned int id; // add vertices with json handle.
+};
+
 using Graph = boost::adjacency_list<
   boost::vecS,
   boost::vecS,
-  boost::directedS
+  boost::directedS,
+  VProperties
   >;
 
 using vertex_iterator = boost::graph_traits<Graph>::vertex_iterator;
 using edge_iterator = boost::graph_traits<Graph>::edge_iterator;
 
-namespace std{
-  vertex_iterator begin(std::pair<vertex_iterator, vertex_iterator> pair);
-  vertex_iterator end(std::pair<vertex_iterator, vertex_iterator> pair);
-  edge_iterator begin(std::pair<edge_iterator, edge_iterator> pair);
-  edge_iterator end(std::pair<edge_iterator, edge_iterator> pair);
+template <typename T>
+struct _range_pair {
+  std::pair<T,T> pair;
+  _range_pair(std::pair<T,T> p) : pair(p) {}
+  T begin(void) { return pair.first; }
+  T end(void) { return pair.second; }
+};
+template <typename T>
+_range_pair<T> range_pair(std::pair<T,T> p) {
+  return _range_pair<T>(p);
 }
